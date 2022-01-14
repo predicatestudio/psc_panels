a05c0cdc8cc84ed4e6a62759f6f7822c7cb7ee34
BenjaminEchelmeier <benjaminechlmeier@gmail.com>
2022-01-14 16:21:11 -0500

    cleaned things up. serve.py now only statically serves panels.

--- serve.py
+++ serve.py
@@ -5,11 +5,11 @@ from pathlib import Path
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="."), name="static")
 template = Jinja2Templates(".")
 
 
 def get_panels():
+    """Dynamically searches for panels with an example.html"""
     panels = []
     for dir in [obj for obj in Path(__file__).parent.iterdir() if obj.is_dir()]:
         if dir.joinpath("example.html").exists():
@@ -17,6 +17,11 @@ def get_panels():
     return panels
 
 
+# Mount panel dirs as static files
+for panel in get_panels():
+    app.mount("/" + panel, StaticFiles(directory=panel), name=panel)
+
+# Index with links to examples
 @app.get("/")
 def home(request: Request):
     return template.TemplateResponse(
